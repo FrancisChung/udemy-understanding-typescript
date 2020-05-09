a8dd9d62ffb67a35ccf341dd1cf3f0a4d7986a0d
Francis Chung <francis.c.chung@gmail.com>
2020-05-10 05:48:43 +1000

    Array Types

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var person = {
     name: 'Francis',
-    age: 18
+    age: 46,
+    hobbies: ['Basketball', 'Video Games']
 };
+var activities;
+activities = ["Basketball", "Volleyball", "Tennis"];
 console.log(person);
+for (var _i = 0, activities_1 = activities; _i < activities_1.length; _i++) {
+    var act = activities_1[_i];
+    console.log(act.toLowerCase());
+}
--- app.ts
+++ app.ts
@@ -1,9 +1,14 @@
-const person: {
-    name:string;
-    age: number;
-} = {
+const person = {
     name: 'Francis',
-    age: 18
+    age: 46,
+    hobbies: ['Basketball', 'Video Games']
 }
 
+let activities: string[];
+activities = ["Basketball", "Volleyball", "Tennis"];
+
 console.log(person);
+
+for (const act of activities) {
+    console.log(act.toLowerCase());
+}
