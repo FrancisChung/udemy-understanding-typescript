c8fb3bd1bbb63e91e0b533798dd776ccc52e2361
Francis Chung <francis.c.chung@gmail.com>
2020-05-11 06:16:01 +1000

    Commented out Basics Module

--- basics.ts
+++ basics.ts
@@ -1,4 +1,4 @@
-function add(n1 : number, n2 : number, showResult: Boolean, phrase: string ) {
+/* function add(n1 : number, n2 : number, showResult: Boolean, phrase: string ) {
     
     const result = n1 + n2;
 
@@ -14,4 +14,4 @@ const number2 = 2.8;
 const printResult = true;
 const resultPhrase = "Result is : ";
 
-const result = add(number1, number2, printResult, resultPhrase);
+const result = add(number1, number2, printResult, resultPhrase); */
