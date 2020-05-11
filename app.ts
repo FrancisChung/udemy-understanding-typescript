2a1183baabe53143d238b0c7d3fbff34ed8a9cb9
Francis Chung <francis.c.chung@gmail.com>
2020-05-11 20:30:32 +1000

    Function Types + Callbacks

--- app.ts
+++ app.ts
@@ -6,6 +6,11 @@ function  printResult(num: number):void {
     console.log('Result: ' + num);
 }
 
+function addthenHandle(n1 : number, n2: number, cb :(number) => void) {
+    const result = n1 + n2;
+    cb(result);
+}
+
 printResult(add(5,12));
 
 let combineValues: (a: number, b: number) => number;
@@ -13,3 +18,7 @@ let combineValues: (a: number, b: number) => number;
 combineValues= add;
 
 console.log(combineValues(8,8));
+
+addthenHandle(10, 20, (result) => {
+    console.log(result);
+})
