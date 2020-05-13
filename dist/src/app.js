38c3e4eaf1d96f2880930c1467bb91814a686e08
Francis Chung <francis.c.chung@gmail.com>
2020-05-13 20:08:36 +1000

    outDir, rootDir set

---
+++ dist/src/app.js
@@ -0,0 +1,15 @@
+"use strict";
+let userInput; //restrictive version of any
+let userName;
+userInput = 51;
+userInput = "Max";
+if (typeof userInput == 'string') {
+    userName = userInput;
+}
+//Never is a newer version of void  
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+const result = generateError('An Error Occurred', 500);
+console.log();
+//# sourceMappingURL=app.js.map
