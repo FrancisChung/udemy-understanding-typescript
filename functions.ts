6cf88ecd1a5f10fdbe6a2a2210e250c973cf69a7
Francis Chung <francis.c.chung@gmail.com>
2020-05-13 19:03:06 +1000

    Fixed missing argument

--- functions.ts
+++ functions.ts
@@ -6,7 +6,7 @@ function  printResult(num: number):void {
     console.log('Result: ' + num);
 }
 
-function addthenHandle(n1 : number, n2: number, cb :(number) => void) {
+function addthenHandle(n1 : number, n2: number, cb : (num: number) => void) {
     const result = n1 + n2;
     cb(result);
 }
