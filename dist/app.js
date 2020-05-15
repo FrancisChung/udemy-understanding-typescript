acf881ba8ebc86f453c92f1ada84b7ae305b278f
Francis Chung <francis.c.chung@gmail.com>
2020-05-15 20:57:57 +1000

    Sync app.js

--- dist/app.js
+++ dist/app.js
@@ -1,16 +1,16 @@
 "use strict";
 class Department {
+
     constructor(n) {
         this.name = n;
     }
+
     describe() {
         console.log(`Department: ${this.name}`);
     }
 }
 let dept = new Department("Toy");
 console.log(dept);
+
 dept.describe();
-const toyCopy = { name: "Dummy", describe: dept.describe };
-toyCopy.describe();
-//# sourceMappingURL=app.js.map
 //# sourceMappingURL=app.js.map
