6e89b985b18dfb4db61bf314cd0b74b3bbc5bb97
Francis Chung <francis.c.chung@gmail.com>
2020-05-16 05:41:27 +1000

    Ctor Shorthand, Readonly, Access Modifiers

--- dist/app.js
+++ dist/app.js
@@ -1,16 +1,29 @@
 "use strict";
 class Department {
-
-    constructor(n) {
-        this.name = n;
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employess = [];
     }
-
     describe() {
-        console.log(`Department: ${this.name}`);
+        console.log(`Department: ${this.id} ${this.name}`);
+    }
+    addEmployee(employee) {
+        this.employess.push(employee);
+    }
+    printEmployeeInfo() {
+        console.log(`Length: ${this.employess.length}`);
+        console.log(this.employess);
     }
 }
-let dept = new Department("Toy");
+let dept = new Department("D1", "Toy");
 console.log(dept);
-
 dept.describe();
+dept.addEmployee("Max");
+dept.addEmployee("Nico");
+dept.addEmployee("Manu");
+dept.printEmployeeInfo();
+// const toyCopy = {name: "Dummy", describe: dept.describe};
+// toyCopy.describe();
+//# sourceMappingURL=app.js.map
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,35 @@
 class Department {
-    name: string;
+    private employess: string[] = [];
 
-    constructor(n: string) {
-        this.name = n;
+    constructor(private readonly id:string, public name: string) {
     }
 
     describe(this: Department) {
-        console.log(`Department: ${this.name}`);
+        console.log(`Department: ${this.id} ${this.name}`);
+    }
+
+    addEmployee(employee: string) {
+        this.employess.push(employee);
+    }
+
+    printEmployeeInfo() {
+        console.log(`Length: ${this.employess.length}`);
+        console.log(this.employess);
     }
 }
-let dept = new Department("Toy");
+let dept = new Department("D1", "Toy");
 console.log(dept);
 
 dept.describe();
 
-const toyCopy = {name: "Dummy", describe: dept.describe};
-toyCopy.describe();
+dept.addEmployee("Max");
+dept.addEmployee("Nico");
+dept.addEmployee("Manu");
+
+dept.printEmployeeInfo();
+
+// const toyCopy = {name: "Dummy", describe: dept.describe};
+// toyCopy.describe();
 
 
 //# sourceMappingURL=app.js.map
